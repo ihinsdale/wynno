b50ffc7cbde809ed774af1b505e27f9ed7a03a9c
Hack Reactor Students <github@hackreactor.com>
2013-10-29 17:22:51 -0700

    solved the since_id discrepancy. tweets writing correctly. still need to handle case where >200 tweets have occurred since last fetch, i.e. fetch requests need to be spaced so that rate limit is not hit.

--- controllers/twitter.js
+++ controllers/twitter.js
@@ -1,6 +1,6 @@
 var Tweet = require('../models/Tweet.js').Tweet;
 
-exports.fetchAndSave = function() {
+exports.fetchAndSave = function(callback) {
   // function to synchronously save the tweets in an array to the db
   var saveSync = function(TweetModel, array, counter) {
     if (counter >= 0) {
@@ -14,7 +14,26 @@ exports.fetchAndSave = function() {
           saveSync(TweetModel, array, counter - 1);
         }
       });
+    } else if (callback) {
+      callback();
     }
+  };
+
+  var incStrNum = function(n) { // courtesy of http://webapplog.com/decreasing-64-bit-tweet-id-in-javascript/
+    n = n.toString(); // but n should be passed in as a string
+    var result = n;
+    var i = n.length - 1;
+    while (i > -1) {
+      if (n[i] === "9") {
+        result = result.substring(0,i) + "0" + result.substring(i + 1);
+        i--;
+      }
+      else {
+        result=result.substring(0,i)+(parseInt(n[i],10)+1).toString()+result.substring(i+1);
+        return result;
+      }
+    }
+    return result;
   };
 
   var twit = require('../config/config.js').twit();
@@ -27,7 +46,8 @@ exports.fetchAndSave = function() {
     } else if (item === null) {
       console.log('Collection has no records');
     } else {
-      options.since_id = item.id;
+      options.since_id = incStrNum(item.id_str); // this incrementing performed because since_id is actually inclusive,
+      // contra the Twitter API docs. Cf. https://dev.twitter.com/discussions/11084
       console.log(options);
     }
     twit.get('https://api.twitter.com/1.1/statuses/home_timeline.json', options, function(error, data) {
