8a90206cacf5716b29109a232a01ad4b843e11dd
Hack Reactor Students <github@hackreactor.com>
2013-10-31 19:36:31 -0700

    rendering tweets somewhat elegantly

--- public/app/scripts/controllers/main.js
+++ public/app/scripts/controllers/main.js
@@ -6,14 +6,14 @@ angular.module('wynnoApp')
     .success(function(data, status, headers, config) {
       console.log('success getting old tweets, they look like:', data);
       $scope.tweets = data;
-      $http.get('/new')
-      .success(function(data2, status2, headers2, config2) {
-        console.log('success getting new tweets, they look like:', data2);
-        $scope.tweets = data2.concat($scope.tweets);
-      })
-      .error(function(data2, status2) {
-        console.log('error getting /new, data look like:', data2);
-      });
+      // $http.get('/new')
+      // .success(function(data2, status2, headers2, config2) {
+      //   console.log('success getting new tweets, they look like:', data2);
+      //   $scope.tweets = data2.concat($scope.tweets);
+      // })
+      // .error(function(data2, status2) {
+      //   console.log('error getting /new, data look like:', data2);
+      // });
     })
     .error(function(data, status) {
       console.log('error getting /old, data look like:', data);
