5c8b41974b1ee3b80759b33107d7a22a19232b23
ihinsdale <ihinsdale@gmail.com>
2013-11-06 15:13:59 -0800

    started to set up the classifier

--- python/server.py
+++ python/server.py
@@ -1,10 +1,49 @@
 import pymongo
 import zerorpc
+import nltk
+import random
 
-class HelloRPC(object):
-    def hello(self, name):
-        return "Hello, %s" % name
+from pymongo import MongoClient
+client = MongoClient()
+db = client.test
+tweets = db.tweets
+votedTweets = tweets.find( { "__vote": { "$nin": [None] } } )
 
-s = zerorpc.Server(HelloRPC())
-s.bind("tcp://0.0.0.0:4242")
-s.run()
+def tweet_features(tweet):
+  features = {}
+  features['tweeter'] = tweet['__user']['screen_name']
+
+  # if retweeter
+  features['retweeter'] = tweet['__retweeter']['screen_name']
+
+  return features
+
+
+featureSets = [(tweet_features(tweet), tweet['__vote']) for tweet in votedTweets]
+#random.shuffle(feature_sets)
+
+twoThirds = math.floor(len(featureSets)*2/3)
+halfOfRemaining = math.floor(len(featureSets)*5/6)
+
+trainSet = featureSets[:twoThirds]
+devtestSet = featureSets[twoThirds:halfOfRemaining]
+testSet = featureSets[halfOfRemaining:]
+
+print 'Tweets voted on: ' + str(tweets.find({"__vote": {"$gte": 0}}).count())
+print 'Out of ' + str(tweets.find({"__vote": None}).count()) ' total tweets'
+#for tweet in tweets.find():
+#    print tweet
+print 'hello, world'
+
+# class HelloRPC(object):
+#     def hello(self, name):
+#         return "Hello, %s" % name
+
+# s = zerorpc.Server(HelloRPC())
+# s.bind("tcp://0.0.0.0:4242")
+# s.run()
+
+# take all tweets that have been voted on, divide into training and dev sets
+# where training is 4/5 and dev 1/5
+
+# classification should be done on all non-voted-upon tweets
