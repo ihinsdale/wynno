d685c8618bf3d34dc1baa62f203c8d2aa34fb5dd
ihinsdale <ihinsdale@gmail.com>
2013-11-06 22:00:57 -0800

    need to save guessed classification of non-voted tweets to the database and send results to node server

--- controllers/algo.js
+++ controllers/algo.js
@@ -3,6 +3,6 @@ var zerorpc = require("zerorpc");
 var client = new zerorpc.Client();
 client.connect("tcp://127.0.0.1:4242");
 
-client.invoke("hello", "RPC", function(error, res, more) {
+client.invoke("crunch", "algo", function(error, res, more) {
     console.log(res);
 });
--- python/server.py
+++ python/server.py
@@ -13,17 +13,17 @@ print 'Tweets voted on: ' + str(tweets.find({"__vote": {"$gte": 0}}).count())
 print 'Out of ' + str(tweets.find().count()) + ' total tweets'
 
 votedTweets = tweets.find( { "__vote": { "$nin": [None] } } )
+nonvotedTweets = tweets.find( {"__vote": None})
+print str(nonvotedTweets.count())
 
 def tweet_features(tweet):
   features = {}
   features['tweeter'] = tweet['__user']['screen_name']
 
   if '__retweeter' in tweet:
-    features['isRetweet'] = True
     features['retweeter'] = tweet['__retweeter']['screen_name']
   else:
-    features['isRetweet'] = False
-    features['retweeter'] = None
+    features['retweeter'] = ''
 
   # contains link / more than one
   # contains hashtag / more than one
@@ -34,34 +34,46 @@ def tweet_features(tweet):
 
   return features
 
-featureSets = [(tweet_features(tweet), tweet['__vote']) for tweet in votedTweets]
-random.shuffle(featureSets)
+def crunch(votedTweets, nonvotedTweets):
+  votedFeatureSets = [(tweet_features(tweet), tweet['__vote']) for tweet in votedTweets]
+  random.shuffle(votedFeatureSets)
 
-twoThirds = int(math.floor(len(featureSets)*2/3))
-print 'Size of training set:'
-print twoThirds
-halfOfRemaining = int(math.floor(len(featureSets)*5/6))
-print 'Size of devtest set:'
-print halfOfRemaining - twoThirds
-print 'Size of test set:'
-print len(featureSets) - halfOfRemaining
+  twoThirds = int(math.floor(len(votedFeatureSets)*2/3))
+  print 'Size of training set:'
+  print twoThirds
+  halfOfRemaining = int(math.floor(len(votedFeatureSets)*5/6))
+  print 'Size of devtest set:'
+  print halfOfRemaining - twoThirds
+  print 'Size of test set:'
+  print len(votedFeatureSets) - halfOfRemaining
 
-trainSet = featureSets[:twoThirds]
-devtestSet = featureSets[twoThirds:halfOfRemaining]
-testSet = featureSets[halfOfRemaining:]
+  trainSet = votedFeatureSets[:twoThirds]
+  devtestSet = votedFeatureSets[twoThirds:halfOfRemaining]
+  testSet = votedFeatureSets[halfOfRemaining:]
 
-classifier = nltk.NaiveBayesClassifier.train(trainSet)
-print nltk.classify.accuracy(classifier, devtestSet)
-print nltk.classify.accuracy(classifier, testSet)
+  classifier = nltk.NaiveBayesClassifier.train(trainSet)
+  print nltk.classify.accuracy(classifier, devtestSet)
+  print nltk.classify.accuracy(classifier, testSet)
+  classifier.show_most_informative_features(10)
 
-# errors = []
-# for (tweet, tag) in devtestSet:
-#   guess = classifier.classify(tweet)
-#   if guess != tag:
-#     errors.append( (tag, guess, tweet) )
+  nonvotedFeatureSet = [tweet_features(tweet) for tweet in nonvotedTweets]
+  print classifier.batch_classify(nonvotedFeatureSet)
 
-# for (tag, guess, tweet) in sorted(errors):
-#   print 'correct=%-8s guess=%-8s features=%-30s' % (tag, guess, tweet)
+
+  # errors = []
+  # for (tweet, tag) in devtestSet:
+  #   guess = classifier.classify(tweet)
+  #   if guess != tag:
+  #     errors.append( (tag, guess, tweet) )
+
+  # for (tag, guess, tweet) in sorted(errors):
+  #   print 'correct=%-8s guess=%-8s features=%-30s' % (tag, guess, tweet)
+  return
+
+crunch(votedTweets, nonvotedTweets)
+
+def save_guesses():
+  return
 
 # class HelloRPC(object):
 #     def hello(self, name):
