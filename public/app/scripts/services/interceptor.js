ca7f65c0f81df836786ecafcb86e5d836a23e900
ihinsdale <ihinsdaledev@gmail.com>
2013-12-31 12:43:26 -0800

    Interceptor should be fixed...

--- public/app/scripts/services/interceptor.js
+++ public/app/scripts/services/interceptor.js
@@ -4,11 +4,12 @@ angular.module('wynnoApp')
 .factory('wynnoInterceptor', ['$q', '$location', '$cookieStore', function($q, $location, $cookieStore) {
   var interceptor = {
     responseError: function(rejection) {
-      console.log('rejection obj:', rejection);
-      console.log('401 response from server intercepted, redirecting to /login.');
-      $cookieStore.remove('user');
-      $location.path('/');
-      return $q.reject(rejection);
+      if (rejection.status === 401) {
+        console.log('401 response from server intercepted, redirecting to /login.');
+        $cookieStore.remove('user');
+        $location.path('/');
+        return $q.reject(rejection);
+      }
     }
   };
 
