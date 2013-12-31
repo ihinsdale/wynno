044969a11b7bc574048785dfca8853b4fa19bdf8
ihinsdale <ihinsdaledev@gmail.com>
2013-12-30 16:55:35 -0800

    Logging user_id before sending to Python.

--- controllers/algo.js
+++ controllers/algo.js
@@ -4,6 +4,7 @@ var client = new zerorpc.Client();
 client.connect("tcp://127.0.0.1:4242");
 
 exports.crunchTheNumbers = function(user_id, _id, callback) {
+  console.log('user_id for sending to Python looks like:', user_id);
   client.invoke("predict", user_id, function(error, res, more) {
     if (error) {
       console.log('there was an error:', error)
