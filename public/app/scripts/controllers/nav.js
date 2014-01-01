204df9a8c605f36d5ffbec82808c774cee3bfbe2
ihinsdale <ihinsdaledev@gmail.com>
2013-12-31 18:00:39 -0800

    $scope.viewing not quite working as intended.

--- public/app/scripts/controllers/nav.js
+++ public/app/scripts/controllers/nav.js
@@ -5,17 +5,21 @@ angular.module('wynnoApp.controllers')
   $scope.activeRequest = true;
   $scope.currentPathNeedsAuth = false;
   console.log('path is:', $location.path());
+
+  // Keep track of which page the user is on
   if ($location.path === '/in') {
+    console.log('current path is /in');
     $scope.viewing = 'passing';
     $scope.active = [true, false, false];
   } else if ($location.path === '/out') {
+    console.log('current path is /out');
     $scope.viewing = 'failing';
     $scope.active = [false, true, false];
   } else if ($location.path === '/settings') {
+    console.log('current path is /settings');
     $scope.viewing = 'settings';
     $scope.active = [false, false, true];
   }
-
   $scope.viewPassing = function() {
     if ($scope.viewing !== 'passing') {
       $scope.viewing = 'passing';
