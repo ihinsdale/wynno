c926fed9cb2b9497e0a4540ce540d624df490b48
ihinsdale <ihinsdaledev@gmail.com>
2014-01-31 15:31:54 -0800

    Debugging.

--- public/app/scripts/services/settings.js
+++ public/app/scripts/services/settings.js
@@ -40,43 +40,43 @@ angular.module('wynnoApp.services')
       // filter cannot apply to all users without at least one condition
       } else if (!draftFilter.users.length && !draftFilter.conditions[0].type) {
         d.reject('At least one user or condition must be specified.');
-      }
-
-      // clean draftFilter of any unnecessary input created by switching condition types
-      for (var i = 0, i < draftFilter.conditions.length; i++) {
-        if (draftFilter.conditions[i].type === 'link') {
-          delete draftFilter.conditions[i].hashtag;
-        } else if (draftFilter.conditions[i].type === 'hashtag') {
-          delete draftFilter.conditions[i].link;
+      } else {
+        // clean draftFilter of any unnecessary input created by switching condition types
+        for (var i = 0, i < draftFilter.conditions.length; i++) {
+          if (draftFilter.conditions[i].type === 'link') {
+            delete draftFilter.conditions[i].hashtag;
+          } else if (draftFilter.conditions[i].type === 'hashtag') {
+            delete draftFilter.conditions[i].link;
+          }
         }
-      }
 
-      // update filters on the client side, to be undone if POST request fails
-      var orig = service.settings.activeFilters.slice();
-      service.settings.activeFilters.push(draftFilter);
-      // remove previous version, if this save was a revision
-      if (originalIndex) {
-        service.settings.activeFilters.splice(originalIndex, 1);
-      }
+        // update filters on the client side, to be undone if POST request fails
+        var orig = service.settings.activeFilters.slice();
+        service.settings.activeFilters.push(draftFilter);
+        // remove previous version, if this save was a revision
+        if (originalIndex) {
+          service.settings.activeFilters.splice(originalIndex, 1);
+        }
 
-      // now POST the new filter
-      var revisionOfFilter_id = originalIndex ? service.settings.activeFilters[originalIndex]._id : null;
-      $http({ method: 'POST', url: '/savefilter', data: {
-        draftFilter: draftFilter,
-        revisionOfFilter_id: revisionOfFilter_id
-      } })
-      .success(function(data, status) {
-        console.log('Success saving filter.');
-        // apply the new filters to currentTweets
-        FilterService.applyFilterRules(TweetService.currentTweets, service.settings);
-        d.resolve(service.settings);
-      })
-      .error(function(reason, status) {
-        console.log('Error saving filter.');
-        // reset to original filters
-        service.settings.activeFilters = orig;
-        d.reject(reason);
-      });
+        // now POST the new filter
+        var revisionOfFilter_id = originalIndex ? service.settings.activeFilters[originalIndex]._id : null;
+        $http({ method: 'POST', url: '/savefilter', data: {
+          draftFilter: draftFilter,
+          revisionOfFilter_id: revisionOfFilter_id
+        } })
+        .success(function(data, status) {
+          console.log('Success saving filter.');
+          // apply the new filters to currentTweets
+          FilterService.applyFilterRules(TweetService.currentTweets, service.settings);
+          d.resolve(service.settings);
+        })
+        .error(function(reason, status) {
+          console.log('Error saving filter.');
+          // reset to original filters
+          service.settings.activeFilters = orig;
+          d.reject(reason);
+        });
+      }
       return d.promise;
     },
     disableFilter: function(index) {
