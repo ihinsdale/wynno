cbfc839c22d71568a0ad161caf2347f69d839a51
ihinsdale <ihinsdaledev@gmail.com>
2014-02-04 10:41:06 -0800

    Fixed a bug in quotation searching.

--- controllers/rendering.js
+++ controllers/rendering.js
@@ -14,7 +14,7 @@ var handleType = function(type, tweet) {
         }  // so that we always search for items[i].url after our last insertion (assumes urls come in order from Twitter API, which they appear to be)
         start = tweet.__text.indexOf(items[i].url, after);
         end = start + items[i].url.length;
-        tweet.__text = replaceAt(tweet.__text, start, end, '<a href="' + items[i].url + '" target="_blank">' + items[i].display_url + '</a>');
+        tweet.__text = replaceAt(tweet.__text, start, end, "<a href='" + items[i].url + "' target='_blank'>" + items[i].display_url + "</a>");
       }
     } else if (type === 'user_mentions') {
       for (var i = 0; i < items.length; i++) {
@@ -25,7 +25,7 @@ var handleType = function(type, tweet) {
         }  // so that we always search for items[i].screen_name after our last insertion (assumes screen_names come in order from Twitter API, which they appear to be)
         start = tweet.__text.indexOf('@', after) + 1;
         end = start + items[i].screen_name.length;
-        tweet.__text = replaceAt(tweet.__text, start, end, '<a href="https://twitter.com/' + items[i].screen_name + '" target="_blank">' + items[i].screen_name + '</a>');
+        tweet.__text = replaceAt(tweet.__text, start, end, "<a href='https://twitter.com/" + items[i].screen_name + "' target='_blank'>" + items[i].screen_name + "</a>");
       }
     }
   }
