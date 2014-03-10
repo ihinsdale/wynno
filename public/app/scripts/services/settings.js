3d1d7b45d7bf8af515484993a4f8fcc2ad2679cb
ihinsdale <ihinsdaledev@gmail.com>
2014-03-10 11:15:03 +0800

    Error fix.

--- public/app/scripts/services/settings.js
+++ public/app/scripts/services/settings.js
@@ -45,7 +45,7 @@ angular.module('wynnoApp.services')
           } else if (draftFilter.conditions[j].type === 'hashtag') {
             // strip a leading # character from the hashtag input
             // this is necessary because I could not get the Bootstrap # input add-on to display inline
-            if (draftFilter.conditions[j].hashtag[0] === '#') {
+            if (draftFilter.conditions[j].hashtag && draftFilter.conditions[j].hashtag[0] === '#') {
               draftFilter.conditions[j].hashtag = draftFilter.conditions[j].hashtag.slice(1);
             }
             delete draftFilter.conditions[j].link;
