807d633f40529c24fb5c678d8081b2074f7c65c5
ihinsdale <ihinsdaledev@gmail.com>
2014-03-18 00:43:34 +0800

    Rendering of hashtag html should work now.

--- controllers/rendering.js
+++ controllers/rendering.js
@@ -13,7 +13,7 @@ var handleType = function(type, tweet) {
           if (i === 0) {
             after = 0;
           } else {
-            after = start + 30 + items[i-1].url.length + items[i-1].display_url.length;
+            after = start + 30 + items[i-1].url.length + items[i-1].display_url.length; // 30 is length of the static link html minus 1
           }  // so that we always search for items[i].url after our last insertion (assumes urls come in order from Twitter API, which they appear to be)
           start = tweet.renderedText.indexOf(items[i].url, after);
           end = start + items[i].url.length;
@@ -30,6 +30,18 @@ var handleType = function(type, tweet) {
           start = tweet.renderedText.indexOf('@', after) + 1;
           end = start + items[i].screen_name.length;
           tweet.renderedText = replaceAt(tweet.renderedText, start, end, "<a href='https://twitter.com/" + items[i].screen_name + "' target='_blank'>" + items[i].screen_name + "</a>");
+        }
+        break;
+      case 'hashtags':
+        for (var i = 0; i < items.length; i++) {
+          if (i === 0) {
+            after = 0;
+          } else {
+            after = start + 71 + 2 * items[i-1].text.length; // 71 is the length of the static link html minus 1 (--minus 1 because of zero-indexing)
+          }  // so that we always search for items[i].screen_name after our last insertion (assumes screen_names come in order from Twitter API, which they appear to be)
+          start = tweet.renderedText.indexOf('@', after) + 1;
+          end = start + items[i].text.length;
+          tweet.renderedText = replaceAt(tweet.renderedText, start, end, "<a href='https://twitter.com/search?q=%23" + items[i].text + "&src=hash' target='_blank'>" + items[i].text + "</a>");
         }
         break;
     }
