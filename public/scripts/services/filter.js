a32a9f92110ebdf36de4b5cf83397527cd5f18af
ihinsdale <ihinsdaledev@gmail.com>
2014-03-24 12:13:28 +0800

    It appears the problem is actually with getting the url.

--- public/scripts/services/filter.js
+++ public/scripts/services/filter.js
@@ -124,8 +124,9 @@ angular.module('wynnoApp.services')
                   // if tweet links to the specified domain, pass
                   for (var m = 0; m < urlsCopyCopy.length; m++) {
                     if (!linkResult) {
+                      console.log('item in urlsCopyCopy is:', urlsCopyCopy[m]);
                       console.log('url is:', urlsCopyCopy[m].extended_url);
-                      host = service.parseUri(urlsCopyCopy[m].extended_url);
+                      host = service.parseUri(urlsCopyCopy[m].extended_url).host;
                       console.log('hostname is:', host);
                       // it appears extended_url always has the same domain as display_url, so we can search
                       // extended_url which has the benefit that we can use parseUri
