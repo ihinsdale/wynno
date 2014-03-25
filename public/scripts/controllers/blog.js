b0a439a825a3cd065a9f7d4c8b2fbaff9cfc3941
ihinsdale <ihinsdaledev@gmail.com>
2014-03-25 13:36:47 +0800

    Setting up blog that uses Markdown and static JSON file as a 'database'.

---
+++ public/scripts/controllers/blog.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('wynnoApp.controllers')
+.controller('BlogCtrl', function($location, BlogService) {
+  var currentLocation = $location.path();
+  if (currentLocation === '/blog') {
+    BlogService.getPosts()
+    .then(function(posts) {
+      $scope.posts = posts;
+    }, function(reason) {
+      console.log('Error grabbing blog posts.');
+      $scope.error = 'There was an error loading the blog.';
+    });
+  } else {
+    var slug = currentLocation.slice(6);
+    BlogService.getPosts()
+    .then(function(posts) {
+      $scope.post = posts[BlogService.lookup[slug]];
+    }, function(reason) {
+      $scope.error = 'There was an error loading the article.';
+    });
+  }
+});
---
+++ public/views/blog/filtering-basics.md
@@ -0,0 +1,9 @@
+Here are some of the basics of creating and using effective filters in wynno.
+
+Blah blah blah Blah blah blah Blah blah blah Blah blah blah Blah blah blah Blah blah blah 
+Blah blah blah Blah blah blah Blah blah blah Blah blah blah 
+Blah blah blah Blah blah blah Blah blah blah 
+
+Blah blah blah Blah blah blah Blah blah blah Blah blah blah 
+Blah blah blah Blah blah blah Blah blah blah Blah blah blah Blah blah blah 
+Blah blah blah Blah blah blah Blah blah blah 
