7a57e4b81170746f010efc6b22134c1054ad5a91
ihinsdale <ihinsdaledev@gmail.com>
2014-03-29 15:21:05 +0800

    Perfecting styling of filter builder.

--- public/scripts/controllers/nav.js
+++ public/scripts/controllers/nav.js
@@ -75,7 +75,8 @@ angular.module('wynnoApp.controllers')
   $scope.openFeedback = function() {
     var modalInstance = $modal.open({
       templateUrl: '/views/feedback.html',
-      controller: 'FeedbackModalInstanceCtrl'
+      controller: 'FeedbackModalInstanceCtrl',
+      windowClass: 'noFade'
     });
     modalInstance.result.then(function(modalResult) {
       console.log('sending feedback', modalResult.feedback, 'and email', modalResult.email);
@@ -94,7 +95,8 @@ angular.module('wynnoApp.controllers')
   $scope.openWelcome = function() {
     var modalInstance = $modal.open({
       templateUrl: '/views/welcome.html',
-      controller: 'WelcomeModalInstanceCtrl'
+      controller: 'WelcomeModalInstanceCtrl',
+      windowClass: 'noFade'
     });
     modalInstance.result.then(function(modalResult) {
       console.log('sending agreement to ToS', modalResult.agreement);
