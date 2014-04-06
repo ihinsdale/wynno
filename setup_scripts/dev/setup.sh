e18621d2022797ec516561e00cccb813d5fa3546
ihinsdale <ihinsdaledev@gmail.com>
2014-04-06 13:48:47 +0800

    Tweaked landing copy a bit.

--- setup_scripts/dev/setup.sh
+++ setup_scripts/dev/setup.sh
@@ -11,6 +11,8 @@ sudo apt-get install screen
 #sudo apt-get install lvm2
 #sudo apt-get install mdadm
 
+sudo apt-get install fail2ban
+
 # Install MongoDB
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
