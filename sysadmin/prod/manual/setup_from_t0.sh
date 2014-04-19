1b55c71427417bf2ce4c40aaab5990eabe3e712f
ihinsdale <ihinsdaledev@gmail.com>
2014-04-19 14:06:08 -0700

    Stupid mistake.

--- sysadmin/prod/manual/setup_from_t0.sh
+++ sysadmin/prod/manual/setup_from_t0.sh
@@ -124,7 +124,7 @@ python -c "import pyhelpers; pyhelpers.update_json_keys_ips()"
 echo "Enter the filename of the playbook you wish to run:"
 read playbook
 
-ansible-playbook -i production ../$playbook --ask-vault-pass
+ansible-playbook -i production ../$playbook --ask-vault-pass -vvvv
 
 
 # 8. With the servers up and ready, use AWS Route 53 to point the DNS record for (www.)wynno.com to the nginx1 droplet
