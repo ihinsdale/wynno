8a300f52fc6902d26d7583d7fa25fb5891d017e9
ihinsdale <ihinsdaledev@gmail.com>
2014-04-23 11:10:51 -0700

    More debugging.

--- sysadmin/prod/manual/setup_app_from_t0.sh
+++ sysadmin/prod/manual/setup_app_from_t0.sh
@@ -88,8 +88,8 @@ python -c "import pyhelpers; pyhelpers.create_droplets()"
 # 5. Generate the inventory file based on these new droplets
 
 #      Wait a few seconds for the new droplets to all get assigned IP addresses
-echo "Waiting two minutes for creation of new DO droplets to finish."
-sleep 2m
+echo "Waiting three minutes for creation of new DO droplets to finish."
+sleep 3m
 
 #      (The Ansible digital_ocean plugin was able on my Mac to do this using:
 #          python digital_ocean --droplets | python -mjson.tool > dynamic_inventory.json
