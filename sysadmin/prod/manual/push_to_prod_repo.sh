ad0dcc3ebd907332d62f889fbc3edeb0b421e942
ihinsdale <ihinsdaledev@gmail.com>
2014-04-28 21:40:25 -0700

    Hardened committing to wynno-prod-admin by ensuring that files are only copied from a cloned version of wynno repo, so that we know no uncommitted or non-master-branch code can get put in wynno-prod-admin.

--- sysadmin/prod/manual/push_to_prod_repo.sh
+++ sysadmin/prod/manual/push_to_prod_repo.sh
@@ -5,9 +5,11 @@
 # its own repo (wynno-prod-admin) which is then pushed to GitHub and
 # can then be pulled from the wynno-gateway instance
 
-# Set the paths here to the wynno and wynno-prod-admin repos
+# Set the paths here to the wynno (i.e. the source) and wynno-prod-admin repos
+
 PATH_TO_WYNNO_PROD_ADMIN_REPO="/Users/ian/Documents/development/wynno/wynno-prod-admin"
-PATH_TO_WYNNO_REPO="/Users/ian/Documents/development/wynno/wynno"
+PATH_TO_TEMP_WYNNO_CLONE_DIR="/Users/ian/Documents/development/wynno/temp-wynno-clone"
+PATH_TO_WYNNO_SOURCE_REPO="${PATH_TO_TEMP_WYNNO_CLONE_DIR}/wynno"
 
 # The wynno-prod-admin repo consists of everything in /dist which is not
 # gitignored and everything in sysadmin/prod which is not gitignored.
@@ -19,19 +21,32 @@ rm $PATH_TO_WYNNO_PROD_ADMIN_REPO/README.md
 rm -rf $PATH_TO_WYNNO_PROD_ADMIN_REPO/dist
 rm -rf $PATH_TO_WYNNO_PROD_ADMIN_REPO/sysadmin
 
-# Next we copy over everything
-cp -R $PATH_TO_WYNNO_REPO/dist $PATH_TO_WYNNO_PROD_ADMIN_REPO
+# Next we clone the wynno repository into /temp-wynno-clone
+# Cloning wynno insures that what gets put in wynno-prod-admin is the last commit
+# to the master branch of the wynno repo. This eliminates the risk of copying to
+# wynno-prod-admin any uncommitted or non-master-branch changes that have been made
+# to the wynno repo
+cd $PATH_TO_TEMP_WYNNO_CLONE_DIR
+git clone git@github:ihinsdale/wynno.git # note this requires an entry for HostName github in ~/.ssh/config
+cd wynno
+git checkout master
+
+# Next we copy over the necessary files from our temp-wynno-clone
+cp -R $PATH_TO_WYNNO_SOURCE_REPO/dist $PATH_TO_WYNNO_PROD_ADMIN_REPO
 mkdir $PATH_TO_WYNNO_PROD_ADMIN_REPO/sysadmin
-cp -R $PATH_TO_WYNNO_REPO/sysadmin/prod $PATH_TO_WYNNO_PROD_ADMIN_REPO/sysadmin
+cp -R $PATH_TO_WYNNO_SOURCE_REPO/sysadmin/prod $PATH_TO_WYNNO_PROD_ADMIN_REPO/sysadmin
 
 # Then we remove files listed in gitignore
+# (Which is actually unnecessary because since we are using a cloned copy of wynno repo as the source
+# and that cloned copy hasn't had any gitignored things copied into it, there are no files that will be
+# removed by this step. Still, we'll leave it just for security.)
 while read line; do
   rm -r $PATH_TO_WYNNO_PROD_ADMIN_REPO/$line
-done < $PATH_TO_WYNNO_REPO/.gitignore
+done < $PATH_TO_WYNNO_SOURCE_REPO/.gitignore
 
 cd $PATH_TO_WYNNO_PROD_ADMIN_REPO
 git add --all .
-echo "Enter a commit message for this push to the wynno-prod-admin repo master branch:"
-read commit_message
-git commit -m "$commit_message"
+last_line_of_master_log=`tail -r -n 1 $PATH_TO_WYNNO_SOURCE_REPO/.git/logs/refs/heads/master`
+last_commit_hash="$( cut -d ' ' -f 2 <<< $last_line_of_master_log )"
+git commit -m "Through master commit ${last_commit_hash}"
 git push origin master
